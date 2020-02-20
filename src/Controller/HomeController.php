dff293ea419bf27e5964fe884599d3ffb477b8b5
Sithis <ma.sithis@gmail.com>
2020-02-20 13:51:31 +0100

    sending dev project

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        if (($user = $this->getUser()))
+        {
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+                'user_name' => $user->getUsername(),
+            ]);
+        }
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+//Entity
+use App\Entity\User;
+//Entity
+
+//Password encoder
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+//Password encoder
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('root');
+        $user->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
+        $password="root";
+
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+        $manager->flush();
+    }
+}
